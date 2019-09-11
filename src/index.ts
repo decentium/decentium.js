414eda833948e295b2d3088903786e0928a04e8d
Johan Nordberg <its@johan-nordberg.com>
2019-09-11 17:17:15 +0200

    Expose API classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { default as render } from './render'
+export {ApiClient} from './api-client'
+export {DataProvider, TableQuery, TableResponse, Action, Transaction, Block} from './data-providers'
+export {default as render} from './render'
+export {EosjsDataProvider, EosjsDataProviderOptions} from './data-providers/eosjs'
